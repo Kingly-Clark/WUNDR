b62185725ed4438cd0a857dec2f5df1027928056
Hamza Tariq <hamzatrq116@gmail.com>
2023-01-18 02:25:27 +0500

    Updating total supply

--- wunderpar_ERC20.sol
+++ wunderpar_ERC20.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
     constructor() ERC20("wunderpar", "WUNDR") {
-        _mint(msg.sender, 1000);
+        _mint(msg.sender, uint256(10**6 * 10**18)); // minting 1 million tokens with 18 decimals
     }
 }
