c4915a4632caa3a82ce7fa68102ed61401bca45b
Hamza Tariq <hamzatrq116@gmail.com>
2023-01-18 02:23:46 +0500

    Update wandurparRewards.sol

--- wandurparRewards.sol
+++ wandurparRewards.sol
@@ -14,17 +14,17 @@ contract wandurparRewards is Ownable, AccessControl {
     
     bytes32 adminRole = keccak256("Admin");
 
-    uint256 _cooldownTimeperiod = 1 days;
-    mapping (address => uint256) public userCooldown;
     mapping (address => uint256) public userEarnings;
 
     event RewardSent(address[] rewardAddresses, uint256[] amounts, uint256 timestamp);
+    event MaxAmountUpdated(uint256 oldAmount, uint256 newAmount, uint256 timestamp);
+
+    uint256 private _maxTokensPerUserPerTransaction = 240 * 10 ** 18; // 240 tokens
 
     constructor (IERC20 rewardsERC20) {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _setupRole(adminRole, msg.sender);
         _setRoleAdmin(adminRole, DEFAULT_ADMIN_ROLE);
-
         _rewardsERC20 = rewardsERC20;       
     }
 
@@ -36,24 +36,25 @@ contract wandurparRewards is Ownable, AccessControl {
         _rewardsERC20 = rewardsERC20;
     }
 
+    function getMaxTransactionAmount() public view returns(uint256) {
+        return _maxTokensPerUserPerTransaction;
+    }
+
+    function setMaxTransactionAmount(uint256 amount) public onlyRole(adminRole) {
+        uint256 oldAmount = _maxTokensPerUserPerTransaction;
+        _maxTokensPerUserPerTransaction = amount;
+        emit MaxAmountUpdated(oldAmount, amount, block.timestamp);
+    }
+
     function checkPoolTokens() public view returns (uint256) {
         return _rewardsERC20.balanceOf(address(this));
-    }
-
-    function setCooldown(uint256 cooldownTime) public onlyOwner {
-        _cooldownTimeperiod = cooldownTime;
-    }
-
-    function getCooldown() public view returns(uint256) {
-        return _cooldownTimeperiod;
     }
 
     function sendRewards(address[] memory rewardAddresses, uint256[] memory amounts) public onlyRole(adminRole) {
         require(rewardAddresses.length == amounts.length, "length of addresses don't match the the amounts sent");
         for (uint256 i = 0; i < rewardAddresses.length; i++) {
-            require(block.timestamp >= userCooldown[rewardAddresses[i]] + _cooldownTimeperiod, "One of the addresses is still under cooldown timeperiod");
+            require(amounts[i] < _maxTokensPerUserPerTransaction, "Amount is greater than the allowed limit");
             require(_rewardsERC20.transfer(rewardAddresses[i], amounts[i]));
-            userCooldown[rewardAddresses[i]] = block.timestamp;
             userEarnings[rewardAddresses[i]] += amounts[i];
         }
         emit RewardSent(rewardAddresses, amounts, block.timestamp);
